f720649a2f5e006b1952c5bc7a8ab59a14de05bb
nikodani3006-ai <nikodani3006@gmail.com>
2025-11-17 19:20:38 +0400

    commit

---
+++ level 14/homework/HW5.py
@@ -0,0 +1,8 @@
+# მომხმარებელს შემოატანინეთ რიცხვი სანამ თქვენს მიერ ჩაფიქრებულ რიცხვს არ შემოიტანს.
+
+number = str(input("Enter number:"))
+
+while number != "4" :
+      print("არასწორია")
+      number = str(input("Enter number:"))
+print("სწორია")
---
+++ level 14/homework/HW9.py
@@ -0,0 +1,6 @@
+# მომხმარებლის შემოტანილი რიცხვის ჩათვლით გამოიტანეთ ყველა რიცხვის კვადრატი.
+
+number = int(input("Enter number:"))
+
+for i in range(0,number):
+     print(i*i)
