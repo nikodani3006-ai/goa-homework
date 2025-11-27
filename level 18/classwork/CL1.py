d2ddc08e4f1ba42e7b6d0e7999a237401d38f9b4
nikodani3006-ai <nikodani3006@gmail.com>
2025-11-27 23:06:11 +0400

    commit

---
+++ level 18/classwork/CL1.py
@@ -0,0 +1,12 @@
+
+# შექმენით სია , ამ შიაში შეინახეთ float იც ,boolean იც, string იც , integer იც და ასევე ცალკე სია სადაც შეოინახავ შენს სახელს და ასევე გვარს და ასაკს,დაპრინტე ეს სია რომ ნახო შედეგი
+
+
+names = ["nikoloz ,nagebashvili , 14"]
+
+list = ["false , nika, lasha , gio , 12, 12.9, ",names]
+
+print(list)
+
+
+
