2f074b07d409dc43f474c6b1d58787661cf074b9
nikodani3006-ai <nikodani3006@gmail.com>
2025-12-12 15:23:11 +0400

    commit

---
+++ level 22/homework/HW1.py
@@ -0,0 +1,7 @@
+# დაპრინტე სიის პირველი და ბოლო ელემენტი ინდექსების გამოყენებით.
+
+# fruits = ["apple", "banana", "mango", "kiwi"]
+
+fruits = ["apple", "banana", "mango", "kiwi"]
+print(fruits[0])
+print(fruits[3])
---
+++ level 22/homework/HW12.py
@@ -0,0 +1,15 @@
+# შექმენი ცვლადი სადაც შეინახავ რიცხვებით სავსე სიას
+
+# შენი დავალებაა შეამოწმო -->
+
+# თუ სიაში მყოფი მეორე ინდექსზე მდგომი ელემენტი არის ლუწი
+#     დაპრნტე --> the element on second index is even
+# სხვა შემთხვევაში ---->
+#     დაპრინტე --> the element on second index is odd
+
+list = [75,63,24,83,6004,56]
+
+if list[1] % 2 == 0:
+     print(" the element on second index is even")
+else:
+     print("the element on second index is odd")
