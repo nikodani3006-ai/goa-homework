211a9de64332ed9815b154b9362ce2b73f154f19
nikodani3006-ai <nikodani3006@gmail.com>
2025-12-18 22:52:53 +0400

    commit

---
+++ level 24/classwork/CL2.py
@@ -0,0 +1,4 @@
+# შექმენით ია --> ["goga" , "irakli" ,'giorgi' , "saba" , "gela" ,"daviti"] ტერმინალში გამოიტანეთ irakli, saba და giorgi როგორც დადებით ასევე უარყოფითი ინდექსებით
+
+names = ["goga" , "irakli" ,'giorgi' , "saba" , "gela" ,"daviti"]
+print(names[1] + names[3] + names[2] + names[-5] + names[-3] + names[-4])
---
+++ level 24/classwork/CL3.py
@@ -0,0 +1,6 @@
+# შექმენი სია  --  ["goga" ,"irma" ,"levani" ,"zuka" ,"tamta" ,'girogi', "daviti" ,"emzari", "sulxani"] შენი დავალებაა რომ slicing ის გამოყენებით შექმნა ახალი სია სადაც იქნება მოთავსებული ელემენტები "giorgi daviti emzari და sulxani
+
+list = ["goga" ,"irma" ,"levani" ,"zuka" ,"tamta" ,'girogi', "daviti" ,"emzari", "sulxani"]
+
+slice = list[5:]
+print(slice)
