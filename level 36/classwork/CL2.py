23129d281cd1693210178350161d5a134b868eaf
nikodani3006-ai <nikodani3006@gmail.com>
2026-02-07 18:25:34 +0400

    commit

---
+++ level 36/classwork/CL2.py
@@ -0,0 +1,11 @@
+#  შექმენი ფუქნცია სახელად numbers რომელიც მიიღებს პარამეტრად რაღაც რიცხვს და დაპრინტავს ეს რიცხვი კენტია თუ ლუწი
+
+
+def numbers(n):
+     if n % 2 == 0:
+         print("luwia")
+     else:
+         print("kentia")
+numbers(22)
+numbers(9)
+            
