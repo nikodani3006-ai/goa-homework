b7d82614a68203c6f29481114eb5b7acc5eef20d
nikodani3006-ai <nikodani3006@gmail.com>
2026-03-10 17:31:42 +0400

    commit

---
+++ level 45/homework/codewars.py
@@ -0,0 +1,36 @@
+# codewars 1 
+# def accum(st):
+#     word = ""
+
+#     for i in range(len(st)):
+#         word += st[i].upper() + st[i].lower() * i
+
+#         if i != len(st) - 1:
+#             word += "-"
+
+#     return word
+
+
+
+# codewars 2
+# def litres(time):
+#     return int(time * 0.5)
+
+# codewars 3
+
+
+
+# codewars 4
+
+# def lovefunc( flower1, flower2 ):
+#     if flower1 % 2 == 0 and flower2 % 2 != 0:
+#         return True
+#     elif flower1 % 2 != 0 and flower2 % 2 == 0:
+#         return True
+#     else:
+#         return False
+
+# codewars 5
+
+
+# codewars 6
